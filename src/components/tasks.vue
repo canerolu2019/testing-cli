96f72e279a09dda3c45913dc4379fb9fe48304c8
canerolu2019 <canerolu1@gmail.com>
2020-10-15 19:54:33 +0300

    vue js execitiu

---
+++ src/components/tasks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul>
+            <li>Item 1</li> 
+            <li>Item 2</li>
+        </ul>
+    </div>
+</template> 
+
+<script>
+export default {
+    
+}
+</script> 
+
+<style lang="stylus">
+
+</style>
--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    <tasks/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Tasks from './components/tasks'
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'App',
   components: {
-    Tasks
+    HelloWorld
   }
 }
 </script>
